4209f2e00b4929e595e3e841e47fcb1409dd1791
Christoph <88708549+gvtsch@users.noreply.github.com>
2022-07-11 17:29:00 +0200

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+first_multiple_input = input().rstrip().split()
+n = int(first_multiple_input[0])
+m = int(first_multiple_input[1])
+
+matrix = []
+
+for _ in range(n):
+    matrix_item = input()
+    matrix.append(matrix_item)
+    
+foo = ''
+
+for i in range(m):
+    for j in range(n):
+        foo = foo + matrix[j][i]
+
+print(re.sub("(?<=[a-zA-Z0-9])[!@#$%& ]{1,}(?=[a-zA-Z0-9])", " ", foo))
